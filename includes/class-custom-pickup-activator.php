a8ec0a571ae683bdb43200d9c261fcc9dddff7ec
chaudhari-mithilesh <mithilesh.chaudhari@wisdmlabs.com>
2023-04-06 17:50:54 +0530

    Initial Commit.

---
+++ includes/class-custom-pickup-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://mithilesh.wisdmlabs.net/
+ * @since      1.0.0
+ *
+ * @package    Custom_Pickup
+ * @subpackage Custom_Pickup/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Pickup
+ * @subpackage Custom_Pickup/includes
+ * @author     Mithilesh <mithilesh.chaudhaudhari@wisdmlabs.com>
+ */
+class Custom_Pickup_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-custom-pickup-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://mithilesh.wisdmlabs.net/
+ * @since      1.0.0
+ *
+ * @package    Custom_Pickup
+ * @subpackage Custom_Pickup/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Pickup
+ * @subpackage Custom_Pickup/includes
+ * @author     Mithilesh <mithilesh.chaudhaudhari@wisdmlabs.com>
+ */
+class Custom_Pickup_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
